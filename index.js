95b74b5618d6f18db654713f4145d6a782ed0751
isaac petersen <isaac.petersen5@gmail.com>
2022-01-25 17:08:12 -0800

    everything done besides write-file

--- index.js
+++ index.js
@@ -1,35 +1,109 @@
+//installing required features
 const fs = require('fs');
-const Employee = require('./lib/Employee');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const inquirer = require('inquirer');
-const manager = []
+const managerArr = [];
+const engineersArr = [];
+const internsArr = [];
 
-
-    inquirer.prompt([
-        {
-            type: 'input',
-            message: 'What is the managers name?',
-            name: "managerName"
-        },
-        {
-            type: 'input',
-            message: `What is the manager's employee ID?`,
-            name: "managerId"
-        },
-        {
-            type: 'input',
-            message: 'What is the managers email?',
-            name: "managerEmail"
-        },
-        {
-            type: 'input',
-            message: 'What is the managers office number?',
-            name: "managerOffice"
-        }
-    ]).then(ans => {
-        const newManager = new Manager(ans.managerName, ans.managerId, ans.managerEmail, ans.managerOffice)
-        manager.push(newManager)
-        console.log(manager)
-    })
+//Engineers questions
+const engQuestions = [{
+        type: 'input',
+        message: 'What is the engineers name?',
+        name: 'engName'
+    },
+    {
+        type: 'input',
+        message: 'What is the engineers ID?',
+        name: 'engID'
+    },
+    {
+        type: 'input',
+        message: 'What is the engineers Email?',
+        name: 'engEmail'
+    },
+    {
+        type: 'input',
+        message: 'What is the engineers Github username?',
+        name: 'engGithub'
+    }
+]
+//Interns questions
+const intQuestions = [{
+        type: 'input',
+        message: 'What is the interns name?',
+        name: 'intName'
+    },
+    {
+        type: 'input',
+        message: 'What is the interns ID?',
+        name: 'intID'
+    },
+    {
+        type: 'input',
+        message: 'What is the interns Email?',
+        name: 'intEmail'
+    },
+    {
+        type: 'input',
+        message: 'Where does the intern go to school?',
+        name: 'intGithub'
+    }
+]
+//Starting on node run, asking manager's information
+inquirer.prompt([{
+        type: 'input',
+        message: 'What is the managers name?',
+        name: "managerName"
+    },
+    {
+        type: 'input',
+        message: `What is the manager's employee ID?`,
+        name: "managerID"
+    },
+    {
+        type: 'input',
+        message: 'What is the managers email?',
+        name: "managerEmail"
+    },
+    {
+        type: 'input',
+        message: 'What is the managers office number?',
+        name: "managerOffice"
+    }
+]).then(ans => {
+    const newManager = new Manager(ans.managerName, ans.managerID, ans.managerEmail, ans.managerOffice);
+    managerArr.push(newManager);
+    console.log(managerArr);
+    const questions = () => {
+        inquirer.prompt([{
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: ['Add an engineer', 'Add an intern', 'Quit'],
+            name: 'initial'
+        }]).then(ans => {
+            if (ans.initial === 'Add an engineer') {
+                inquirer.prompt(engQuestions)
+                .then(ans => {
+                    const newEngineer = new Engineer(ans.engName, ans.engID, ans.endEmail, ans.engGithub)
+                    engineersArr.push(newEngineer)
+                    console.log(engineersArr)
+                    questions()
+                })
+            } else if (ans.initial === 'Add an intern') {
+                inquirer.prompt(intQuestions)
+                .then(ans => {
+                    const newIntern = new Intern(ans.intName, ans.intID, ans.intEmail, ans.intGithub)
+                    engineersArr.push(newIntern)
+                    console.log(internsArr)
+                    questions()
+                })
+            } else {
+                // writefile
+            }
+        })
+    };
+questions()
+});
